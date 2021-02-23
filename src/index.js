d48969e54e38e8156291b186638ab805d4d52984
Rasite <hrasiter@gmail.com>
2021-02-24 00:29:55 +0300

    File upload,delete, serve file, file validation saving avatar in user document resize an image before saving

--- src/index.js
+++ src/index.js
@@ -6,18 +6,33 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next)=>{
-//     console.log(req.method, req.path)
-//     if(req.method === 'GET'){
-//         res.send('GET request are disabled')
-//     }else{
-//         next()
+
+// const multer = require('multer')
+// const upload = multer({
+//     dest: 'images',
+//     limits: {
+//         fileSize:1000000, //in bytes
+//     },
+//     fileFilter(req, file, cb){
+//         // cb(new Error('File must be a PDF'))
+//         // cb(undefined, true)
+//         // cb(undefined, false)
+//         if(!file.originalname.match(/\.(doc|docx)$/)){
+//             return cb(new Error('Please upload a doc or docx'))
+//         }
+
+//         cb(undefined, true)
 //     }
-    
 // })
 
-// app.use((req,res,next)=>{
-//     res.status(503).send('Site is currently down. Check back soon!')
+// const errorMiddleware = (req, res, next)=>{
+//     throw new Error('Error from middleware')
+// }
+
+// app.post('/upload', upload.single('upload') , (req, res)=>{
+//     res.send()
+// }, (error, req, res, next)=>{
+//     res.status(400).send({error: error.message})
 // })
 
 app.use(express.json())
@@ -27,30 +42,3 @@ app.use(taskRouter)
 app.listen(port, ()=>{
     console.log('Server is listening on port ' + port)
 })
-
-// const jwt = require('jsonwebtoken')
-
-// const myFunction = async () => {  
-//     const token = jwt.sign({__id: 'asdsadasd'},'thisisthecoursetoken', {expiresIn:'7 days'})  
-//     console.log(token)
-
-//     const data = jwt.verify(token, 'thisisthecoursetoken')
-//     console.log(data)
-// }
-
-// myFunction()
-
-// const Task = require('./models/task')
-// const User = require('./models/user')
-
-// const main = async ()=>{
-//     // const task = await Task.findById('6034097c1c5395126cf7895f')
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
-
-//     const user = await User.findById('603408a40c321811d611e407')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
-// }
-
-//main()
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const multer = require('multer')
+const sharp = require('sharp')
 
 const router = new express.Router()
 
@@ -51,6 +53,29 @@ router.post('/users/logoutAll', auth, async (req,res)=>{
     }
 })
 
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            cb(new Error('Upload a jpg, jpeg or png image'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res)=>{
+    const buffer = await sharp(req.file.buffer).resize({width:250, height:250}).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+
+    res.send()
+}, (error, req, res, next)=>{
+    res.status(400).send({error: error.message})
+})
+
 router.get('/users/me', auth, async (req, res)=>{
     res.send(req.user)
 })
@@ -85,6 +110,32 @@ router.delete('/users/me', auth, async (req, res)=>{
         res.send(req.user) 
     }catch(e){
         res.status(500).send()
+    }
+})
+
+router.delete('/users/me/avatar', auth, async (req, res)=>{
+    try{
+        req.user.avatar = undefined
+        await req.user.save()
+        res.send()
+    }catch(e){
+        res.status(500).send()
+    }
+    
+})
+
+router.get('/users/:id/avatar', async (req, res)=>{
+    try{
+        const user = await User.findById(req.params.id)
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+    }catch(e){
+        res.status(404).send()
+
     }
 })
 
