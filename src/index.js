1aee8d4e39ff8694f64d366dd4a361413cbc7fe5
Rasite <hrasiter@gmail.com>
2021-02-27 01:10:42 +0300

    web client commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const app = require('./app')
+const path = require('path')
+const express = require('express')
 
 const port = process.env.PORT
+
+const publicdir = path.join(__dirname, '../public')
+app.use(express.static(publicdir))
+
+app.get('', (req, res)=>{
+    res.render('index',{
+        title: 'Weather',
+        name: 'Rasit'
+    })
+})
 
 app.listen(port, ()=>{
     console.log('Server is listening on port ' + port)
